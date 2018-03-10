5ce955eb9aa45578bfd764ec2413fee73da85d7f
santosgustavo <gustavosantos510@yahoo.com.br>
2018-03-09 21:26:14 -0300

    Rest simples funcionamento.

---
+++ src/main/java/com/gustavo/cursomc/resource/CategoriaResource.java
@@ -0,0 +1,16 @@
+package com.gustavo.cursomc.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/categoria")
+public class CategoriaResource {
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public String listar() {
+		return "REST Funcionando Porra!";
+	}
+
+}
