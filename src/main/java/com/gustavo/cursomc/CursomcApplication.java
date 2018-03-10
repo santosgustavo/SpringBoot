f6d6d6225adb2683ab83bf5f6abbf48457ba5283
santosgustavo <gustavosantos510@yahoo.com.br>
2018-03-09 22:22:45 -0300

    Hibernate -Gera tabela - annotations

--- src/main/java/com/gustavo/cursomc/CursomcApplication.java
+++ src/main/java/com/gustavo/cursomc/CursomcApplication.java
@@ -2,10 +2,11 @@ package com.gustavo.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
 public class CursomcApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 		
