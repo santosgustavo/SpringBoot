495ab4905023c710174a54a26a82c22db7b5391a
santosgustavo <gustavosantos510@yahoo.com.br>
2018-03-11 15:44:05 -0300

    Operação estanciação - save

--- src/main/java/com/gustavo/cursomc/CursomcApplication.java
+++ src/main/java/com/gustavo/cursomc/CursomcApplication.java
@@ -1,15 +1,36 @@
 package com.gustavo.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import com.gustavo.cursomc.dominio.Categoria;
+
+import br.com.gustavo.cursomc.repositories.CategoriaRepository;
+
 @EnableJpaRepositories(basePackages="br.com.gustavo.cursomc.repositories")
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepositorie;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 		
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepositorie.save(Arrays.asList(cat1, cat2));
 	}
 }
