d3da66cf66c02f11de2e188a2b1a2dc6ff458791
santosgustavo <gustavosantos510@yahoo.com.br>
2018-05-06 19:33:16 -0300

    Lista objeto categoria sem os produtos relacionados usando conceito de DTO

---
+++ src/main/java/com/gustavo/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,36 @@
+package com.gustavo.cursomc.dto;
+
+import com.gustavo.cursomc.dominio.Categoria;
+
+public class CategoriaDTO {
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String nome;
+
+	public CategoriaDTO() {
+
+	}
+
+	public CategoriaDTO(Categoria obj) {
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
